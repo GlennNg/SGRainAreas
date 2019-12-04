c9bde052ac3d837d7e1f1b9c50693fd7cc55850a
Glenn Ng <glenn_ng@Live.com.sg>
2019-12-05 00:37:15 +0800

    Changed alot of shit....

--- index.js
+++ index.js
@@ -2,126 +2,151 @@
 
 const botgram = require("botgram")
 var sharp = require('sharp');
-var fs = require('fs')
+var fs = require('fs');
 var request = require('request');
 var Jimp = require('jimp');
+var http = require('http');
+var Stream = require('stream').Transform;
+var fs = require('fs');
+
 const bot = botgram("1016820507:AAEB2FIcO-tvMkGRVykdDYUq-hnuht7uWNA")
 
+var timeManager = {
+    //predefined deduction
+    deductMinutes: 0,
+    lastUpdatedTime: "0000"
+};
 
-var http = require('http'),
-    Stream = require('stream').Transform,
-    fs = require('fs');
-
-
-
-
-bot.command("start", "help", (msg, reply) =>
-    reply.text("The following are the commands to check out rain areas in Singapore:\n /CheckMeOut"))
-
-bot.command("CheckMeOut", (msg, reply, next) => {
-    reply.text("The image is loading... please wait...");
-
+//useless sleep?!
+async function sleep(msec) {
+    return new Promise(resolve => setTimeout(resolve, msec));
+}
+async function downloadImage(filename) {
     let date_ob = new Date();
-    date_ob.setHours(date_ob.getHours()+8)
-    // adjust 0 before single digit date
-    let date = ("0" + date_ob.getDate()).slice(-2);
-    // current month
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    // current year
-    let year = date_ob.getFullYear();
-    // current hours
+    //setting GMT +8
+    //date_ob.setHours(date_ob.getHours() + 8)
+    date_ob.setHours(date_ob.getHours())
+
+    do {
+        console.log("Executing Do Loop for downloading of new images");
+        date_ob.setMinutes(date_ob.getMinutes() - timeManager.deductMinutes)
+        // adjust 0 before single digit date
+        let date = ("0" + date_ob.getDate()).slice(-2);
+        // current month
+        let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+        // current year
+        let year = date_ob.getFullYear();
+        // current hours
+        let hours = date_ob.getHours();
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        // current minutes
+        let minutes = date_ob.getMinutes();
+        //minutesLO = minutes % 5
+        minutes = minutes - (minutes % 5)
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        var uri = 'http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_' + year + month + date + hours + minutes + '0000dBR.dpsri.png';
+        console.log(uri);
+
+        var promise1 = new Promise(function (resolve, reject) {
+            request.head(uri, function (err, res, body) {
+                //check for err
+                if (err) {
+                    console.log(err)
+                    reject(err);
+                }
+                console.log("Status code: " + res.statusCode);
+                console.log("datetime: " + year + month + date + hours + minutes);
+                //stuck here
+                if (res.statusCode === 200) {
+                    request(uri).pipe(fs.createWriteStream(filename)).on('close', () => {
+                        timeManager.lastUpdatedTime = String(hours + "" + minutes)
+                        console.log("lastUpdatedTime updated with: ", timeManager.lastUpdatedTime)
+                        timeManager.deductMinutes = 0;
+                        resolve();
+                    });
+                }
+                else {
+                    timeManager.deductMinutes = 5;
+                    resolve();
+                }
+            })
+        });
+        const value = await promise1
+        console.log("deductMinutes checker: ", timeManager.deductMinutes)
+        //await sleep(2000);
+    } while (timeManager.deductMinutes > 0)
+};
+
+// console.log("testing download");
+// downloadImage('./Images/currentWeather.png').then(() => console.log('Image successfully downloaded @ ', new Date()));
+
+
+//Pre Cached images
+setInterval(() => {
+    console.log("-----Fetching images-----")
+    let date_ob = new Date();
+    //setting GMT +8
+    //date_ob.setHours(date_ob.getHours() + 8)
+    date_ob.setHours(date_ob.getHours())
     let hours = date_ob.getHours();
     if (hours < 10) {
         hours = "0" + hours;
     }
     // current minutes
     let minutes = date_ob.getMinutes();
-
-    //console.log("unrounded of minutes == " + minutes)
-    minutesLO = minutes % 5
-    minutes = minutes - minutesLO
+    minutes = minutes - (minutes % 5)
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
-    //console.log("rounded of minutes == " + minutes)
+    downloadImage('./Images/currentWeather.png')
+        .then(() => {
+            console.log('Image successfully downloaded @ ', new Date())
+            console.log("./Images/" + String(hours + "" + minutes) + ".jpg")
+            //if (timeManager.lastUpdatedTime !== String(hours+""+minutes)) {
 
-    var url = 'http://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_' + year + month + date + hours + minutes + '0000dBR.dpsri.png';
-    //console.log(url);
-
-    var download = function (uri, filename, callback) {
-	reply.text("Information given below is still in development and may not be accurate");
-	console.log("Update requested by: " + msg.chat.name);
-        request.head(uri, function (err, res, body) {
-            //console.log('content-type:', res.headers['content-type']);
-            //console.log('content-length:', res.headers['content-length']);
-            console.log(res.statusCode);
-            //console.log("User request from: http://t.me/" + msg.chat.username)
-            if (res.statusCode === 404) {
-                console.log("Fetching cached @ " + hours +minutes);
-                var stream = fs.createReadStream("./Images/Cached.jpg");
-                reply.photo(stream, msg.chat.name + ", here's your update. Notice: NEA haven't update. Hence, this is an old update we have");
-                return;
-            }
-            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-        });
-    };
-
-    download(url, './Images/currentWeather.png', () => {
-        //console.log("Fetching no problem.");
-
-        Jimp.read('./Images/currentWeather.png')
-            .then(data => {
-                data
-                    .opacity(0.35)
-                    .write("./Images/currentWeatherNew.png", () => {
-                        if (fs.existsSync("./Images/" +(date%2)+ hours + minutes + ".jpg")) {
-                            //file exists
-                            console.log("Fetching existing image @ " + hours + minutes);
-                            var stream = fs.createReadStream("./Images/Cached.jpg");
-                            reply.photo(stream,msg.chat.name + ", here's your update. Update: Cached copy of last update @ " + hours + minutes);
-                            return;
-                        }
-			//setting weather updates with proper size
-                        sharp("./Images/currentWeatherNew.png")
-                            .resize(853, 479)
-                            .toFile("./Images/currentWeather.png", () => {
-				console.log("the file currentWeather.png exists?" + fs.existsSync("./Images/currentWeather.png"))
-				console.log("The file currentWeatherNew.png exists? "+ fs.existsSync("./Images/currentWeatherNew.png"));
-				//var stream2 = fs.createReadStream("./Images/currentWeather.png");
-				//reply.photo(stream2, "debugging photo");
-				//merging weather with base image
-                                sharp("./assets/base-853.png")
-                                    .composite([{ input: "./Images/currentWeather.png", gravity: "northwest" }, { input: "./assets/MRT.png", gravity: "northwest" }])
-                                    .toFile("./Images/" +(date%2)+ hours + minutes + ".jpg", () => {
-                                        var stream = fs.createReadStream("./Images/" +(date%2)+ hours + minutes + ".jpg");
-					//var stream2 = fs.createReadStream("./Images/currentWeather.png");
-                                        reply.photo(stream, msg.chat.name + ", here's your update. Update: Latest update @ " + hours + minutes);
-					//reply.photo(stream2, "Debugging Photo");
-					//fs.unlinkSync("./Images/currentWeather.png");
-					//fs.unlinkSync("./Images/currentWeatherNew.png");
-					//fs.unlinkSync("./Images/currentWeather1.png");
-                                    })
-                                    .toFile("./Images/Cached.jpg", ()=>{
-					fs.unlinkSync("./Images/currentWeather.png");
-					fs.unlinkSync("./Images/currentWeatherNew.png");
-				});
-                            });
-
-                        console.log("Fetching from NEA @ "+ hours + minutes);
-
-
-                    });// save
-                return data;
-
+            //setOpacity
+            Jimp.read('./Images/currentWeather.png').then(data => {
+                //setOpacity and then write to a new fileName currentWeatherOpacitySet
+                data.opacity(0.35).write('./Images/currentWeatherOpacitySet.png')
+                console.log("currentWeather DONE!")
+            }).then(() => {
+                //ammend currentWeather found into proper size
+                sharp("./Images/currentWeatherOpacitySet.png")
+                    .resize(853, 479)
+                    //save the ammended currentWeather as currentWeatherNew
+                    .toFile("./Images/currentWeatherNew.png", () => {
+                        console.log("currentWeatherNew DONE!")
+                        //merge all the files when it is done.
+                        sharp("./assets/base-853.png")
+                            .composite([{ input: "./Images/currentWeatherNew.png", gravity: "northwest" }, { input: "./assets/MRT.png", gravity: "northwest" }])
+                            .toFile("./Images/" + timeManager.lastUpdatedTime + ".jpg")
+                            .then(() => {
+                                console.log("-----Download completed here-----");
+                            })
+                    })
             })
-            .catch(err => {
-                console.error(err);
-            });
-    });
+            //}
+        })
+    //setting interval at 6mins each time
+}, 30000)
+
+bot.command("start", "help", (msg, reply) => {
+
+    reply.text("The following are the commands to check out rain areas in Singapore:\n/CheckMeOut")
+})
+
+bot.command("CheckMeOut", (msg, reply, next) => {
+    reply.text("Image loading, please wait...");
+    var stream = fs.createReadStream("./Images/" + timeManager.lastUpdatedTime + ".jpg");
+    reply.photo(stream, msg.chat.name + ", here's the update. Latest update @ " + timeManager.lastUpdatedTime + "hrs.");
 
 })
 
 bot.command((msg, reply) => {
     console.log("Testing!");
-    reply.text("Invalid command.")
+    reply.text("Invalid command.");
 })
